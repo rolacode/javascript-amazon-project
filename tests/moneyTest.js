23392f3f8e61329b420e786d5fdf2fca69f7fa76
Rotimi Olanrewaju <rolament4real@gmail.com>
2024-09-29 11:30:30 -0700

    create automated test for formatCurrency

---
+++ tests/moneyTest.js
@@ -0,0 +1,35 @@
+import {formatCurrency} from "../scripts/utils/money.js";
+
+console.log('test suite: formatCurrency');
+
+console.log('check for convert of cents to dollar');
+
+if (formatCurrency(2095) === '20.95') {
+  console.log('passed');
+} else {
+  console.log('failed');
+}
+
+console.log('work with zero');
+
+if (formatCurrency(0) === '0.00') {
+  console.log('passed');
+} else {
+  console.log('failed');
+}
+
+console.log('round up to nearest cents 1');
+
+if (formatCurrency(2000.5) === '20.01') {
+  console.log('passed');
+} else {
+  console.log('failed');
+}
+
+console.log('roundup to nearest cents');
+
+if (formatCurrency(2000.4) === '20.00') {
+  console.log('passed');
+} else {
+  console.log('failed');
+}
