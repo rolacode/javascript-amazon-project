3baf8630513f2bbb3fcc0beb5e1ca55222d78237
Rotimi Olanrewaju <rolament4real@gmail.com>
2025-08-20 18:00:14 -0700

    split header into a component and use Link in place of a-ref

--- ecommerce-frontend/src/App.jsx
+++ ecommerce-frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { HomePage } from './pages/HomePage';
 import { Routes, Route } from 'react-router';
 import './App.css';
+import { Header } from './components/Header';
 import { CheckoutPage } from './pages/CheckoutPage';
 import { OrdersPage } from './pages/OrdersPage';
 import { TrackingPage } from './pages/TrackingPage';
@@ -8,6 +9,7 @@ import { TrackingPage } from './pages/TrackingPage';
 function App() {
   return (
     <Routes>
+      <Route path="/header" element={<Header />} />
       <Route index element={<HomePage />} />
       <Route path="/Checkout" element={<CheckoutPage />} />
       <Route path="/Orders" element={<OrdersPage />} />
