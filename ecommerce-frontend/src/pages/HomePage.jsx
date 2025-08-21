ed16dd78d435a733be7b5268f89d73212a5f096f
Rotimi Olanrewaju <rolament4real@gmail.com>
2025-08-21 14:55:32 -0700

    use data from the backend to generate html

--- ecommerce-frontend/src/pages/HomePage.jsx
+++ ecommerce-frontend/src/pages/HomePage.jsx
@@ -1,13 +1,21 @@
 import axios from 'axios';
+import { useEffect, useState } from "react";
 import { Header } from "../components/Header";
 import "./HomePage.css";
-import { products } from "../../starting-code/data/products";
 
 export function HomePage() {
-    axios.get("http://localhost:3000/api/products")
-        .then((response) => {
-            console.log(response.data);
-        });        
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        // Fetch products data from the API
+        axios.get("http://localhost:3000/api/products")
+            .then((response) => {
+                setProducts(response.data);
+            })
+            .catch((error) => {
+                console.error("Error fetching products:", error);
+            });
+    }, []);    
 
   return (
     <>
