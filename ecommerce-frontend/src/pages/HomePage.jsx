6188643e31a1a47ee13c69e2497b738a956572e7
Rotimi Olanrewaju <rolament4real@gmail.com>
2025-08-21 14:42:43 -0700

    use axios to fetch data

--- ecommerce-frontend/src/pages/HomePage.jsx
+++ ecommerce-frontend/src/pages/HomePage.jsx
@@ -1,15 +1,12 @@
+import axios from 'axios';
 import { Header } from "../components/Header";
 import "./HomePage.css";
 import { products } from "../../starting-code/data/products";
 
 export function HomePage() {
-    fetch("http://localhost:3000/api/products")
-        .then((response) => response.json())
-        .then((data) => {
-            console.log(data);
-        })
-        .catch((error) => {
-            console.error("Error fetching products:", error);
+    axios.get("http://localhost:3000/api/products")
+        .then((response) => {
+            console.log(response.data);
         });        
 
   return (
