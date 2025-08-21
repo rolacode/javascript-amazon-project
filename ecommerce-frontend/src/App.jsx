5873ddee4080a3ebe0213edf3e2fb1fb001659f4
Rotimi Olanrewaju <rolament4real@gmail.com>
2025-08-20 17:25:32 -0700

    create order page and tracking page then I style them.

--- ecommerce-frontend/src/App.jsx
+++ ecommerce-frontend/src/App.jsx
@@ -2,12 +2,16 @@ import { HomePage } from './pages/HomePage';
 import { Routes, Route } from 'react-router';
 import './App.css';
 import { CheckoutPage } from './pages/CheckoutPage';
+import { OrdersPage } from './pages/OrdersPage';
+import { TrackingPage } from './pages/TrackingPage';
 
 function App() {
   return (
     <Routes>
       <Route index element={<HomePage />} />
-      <Route path="/Checkout" element={<CheckoutPage />} /> 
+      <Route path="/Checkout" element={<CheckoutPage />} />
+      <Route path="/Orders" element={<OrdersPage />} />
+      <Route path="/tracking" element={<TrackingPage />} /> 
     </Routes>
   )
 }
