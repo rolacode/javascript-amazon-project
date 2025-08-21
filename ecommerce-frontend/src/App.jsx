da523c2ddd93134c8f98acb60372824c0bf06ce6
Rotimi Olanrewaju <rolament4real@gmail.com>
2025-08-20 18:37:24 -0700

    split the checkout header from the page and also use navlink for better routing

--- ecommerce-frontend/src/App.jsx
+++ ecommerce-frontend/src/App.jsx
@@ -5,11 +5,13 @@ import { Header } from './components/Header';
 import { CheckoutPage } from './pages/CheckoutPage';
 import { OrdersPage } from './pages/OrdersPage';
 import { TrackingPage } from './pages/TrackingPage';
+import { CheckoutHeader } from './components/CheckoutHeader';
 
 function App() {
   return (
     <Routes>
       <Route path="/header" element={<Header />} />
+      <Route path="/checkout-header" element={<CheckoutHeader />} />
       <Route index element={<HomePage />} />
       <Route path="/Checkout" element={<CheckoutPage />} />
       <Route path="/Orders" element={<OrdersPage />} />
---
+++ ecommerce-frontend/src/components/CheckoutHeader.jsx
@@ -0,0 +1,29 @@
+import './checkout-header.css';
+import { NavLink } from 'react-router';
+
+export function CheckoutHeader() {
+    return (
+      <div className="checkout-header">
+        <div className="header-content">
+          <div className="checkout-header-left-section">
+            <NavLink to="/">
+              <img className="logo" src="images/logo.png" />
+              <img className="mobile-logo" src="images/mobile-logo.png" />
+            </NavLink>
+          </div>
+
+          <div className="checkout-header-middle-section">
+            Checkout (
+            <NavLink className="return-to-home-link" to="/">
+              3 items
+            </NavLink>
+            )
+          </div>
+
+          <div className="checkout-header-right-section">
+            <img src="images/icons/checkout-lock-icon.png" />
+          </div>
+        </div>
+      </div>
+    ); 
+}    
